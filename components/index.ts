2b1e39958b9f6278ca320d83d1d74fd2163909be
Youssef Emad <youssefweb120@gmail.com>
2023-07-09 21:27:35 +0300

    first commit

---
+++ components/index.ts
@@ -0,0 +1,20 @@
+import CarCard from "./CarCard";
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import ShowMore from "./ShowMore";
+export {
+  Hero,
+  CustomButton,
+  NavBar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+  CarCard,
+  ShowMore
+};
